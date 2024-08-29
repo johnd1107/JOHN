28b21885e2b01d013eaad5a4adcbf3f59f9be0f6
John <lj.defazd@uea.edu.ec>
2024-08-29 16:18:01 -0500

    Deber de la semana 11 ordenacion de matriz

--- 3.2 Operaciones con Arreglos/ejercio2.py
+++ 3.2 Operaciones con Arreglos/ejercio2.py
@@ -4,3 +4,17 @@ matriz = [
     [2,10,4],
     [6,8,3]
 ]
+print(matriz)
+# Metodo de ordenamiento buble_sort
+def buble_sort(fila):
+    #algoritmo buble_sort
+    n=len(fila)
+    for i in range(n):
+        for j in range(n-1,i,-1):
+            if fila[j]>fila[j-1]:
+                fila[j],fila[j-1]=fila[j-1],fila[j]
+                print(fila)
+print("matriz original")
+print(matriz)
+buble_sort(matriz[0])
+print(matriz)
