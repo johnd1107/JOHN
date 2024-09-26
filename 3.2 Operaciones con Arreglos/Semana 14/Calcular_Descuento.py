7d952af677982117869c89a4e5d4093273fdef0f
John <lj.defazd@uea.edu.ec>
2024-09-25 21:13:23 -0500

    Deber de la semana 15 creacion de diccionario

--- 3.2 Operaciones con Arreglos/Semana 14/Calcular_Descuento.py
+++ 3.2 Operaciones con Arreglos/Semana 14/Calcular_Descuento.py
@@ -5,22 +5,24 @@ def calcular_descuento(monto_total, porcentaje_descuento=10):
     descuento = (monto_total * porcentaje_descuento) / 100
     return descuento
 # Llamadas a la función
-# Primera llamada: porcentaje de descuento por defecto (10%)
+# Descuento del 10%
 monto_total1 = 500
 descuento1 = calcular_descuento(monto_total1)
 monto_final1 = monto_total1 - descuento1
 
-# Segunda llamada: monto total y porcentaje de descuento especificado (20%)
+# Segunda llamada
+# descuento (20%)
 monto_total2 = 1700
 porcentaje_descuento2 = 20
 descuento2 = calcular_descuento(monto_total2, porcentaje_descuento2)
 monto_final2 = monto_total2 - descuento2
 
 # Mostrar los resultados
+# Descuento del 10%
 print(f"Para un monto total de ${monto_total1} descuento del 10%:")
 print(f"Descuento: ${descuento1:.2f}")
 print(f"Monto final a pagar: ${monto_final1:.2f}\n")
-
+# Descuento del 20%
 print(f"Para un monto total de ${monto_total2} descuento del {porcentaje_descuento2}%:")
 print(f"Descuento: ${descuento2:.2f}")
 print(f"Monto final a pagar: ${monto_final2:.2f}")
---
+++ 3.2 Operaciones con Arreglos/Semana15/Diccionario.py
@@ -0,0 +1,24 @@
+# Crear un diccionario con información  personal
+informacion_personal = {
+    "nombre": "Luis Defaz",
+    "edad": 32,
+    "ciudad": "Quito",
+    "profesion": "Estudiante"
+}
+
+# Accede al valor asociado con la clave "ciudad" y modifícalo con una ciudad diferente.
+informacion_personal["ciudad"] = "Cuenca"
+
+# Agregar una nueva clave-valor al diccionario
+informacion_personal["profesion"] = "Aprendiz"
+
+# Verificar si la clave "telefono" existe en el diccionario
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "0997432758"  # Agregar número de teléfono
+
+# Eliminar la clave "edad" del diccionario
+del informacion_personal["edad"]
+
+# Imprimir el diccionario
+for clave, valor in informacion_personal.items():
+    print(f"{clave}: {valor}")
